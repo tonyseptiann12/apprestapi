e394264ab6c47753aed476fc89fc2857c48ae765
tonyseptiann <tonysn@sim.uns.ac.id>
2021-08-29 16:29:04 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,15 @@ exports.tampilSemuaMahasiswa = function (req, res) {
     }
   });
 };
+
+// menampilkan data mahasiswa berdasarkan id
+exports.tampilBerdasarkanId = function (req, res) {
+  let id = req.params.id;
+  connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
