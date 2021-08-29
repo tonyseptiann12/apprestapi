8447b75e821c7d29da4c8ff5bed0d7db6a1cd071
tonyseptiann <tonysn@sim.uns.ac.id>
2021-08-29 20:55:10 +0700

    berhasil menghapus data

--- routes.js
+++ routes.js
@@ -8,4 +8,5 @@ module.exports = function (app) {
   app.route('/tampil/:id').get(jsonku.tampilBerdasarkanId);
   app.route('/tambah').post(jsonku.tambahMahasiswa);
   app.route('/ubah').put(jsonku.ubahMahasiswa);
+  app.route('/hapus').delete(jsonku.hapusMahasiswa);
 };
